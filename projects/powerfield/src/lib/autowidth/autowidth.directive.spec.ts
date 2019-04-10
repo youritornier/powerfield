1e33c4b78951f9a2b5740cc4f9f5aa99facd0702
Youri Tornier <youri.tornier@gmail.com>
2019-04-10 17:46:23 +0200

    Fix: autowith directive and unit tests init

--- projects/powerfield/src/lib/autowidth/autowidth.directive.spec.ts
+++ projects/powerfield/src/lib/autowidth/autowidth.directive.spec.ts
@@ -1,8 +1,75 @@
+import { Component, DebugElement } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+
 import { AutowidthDirective } from './autowidth.directive';
 
+@Component({
+  template: '<input type="text" powAutowidth>'
+})
+class MockComponent {
+  constructor() { }
+}
+
 describe('AutowidthDirective', () => {
-  it('should create an instance', () => {
-    const directive = new AutowidthDirective();
-    expect(directive).toBeTruthy();
+  let component: MockComponent;
+  let fixture: ComponentFixture<MockComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        AutowidthDirective,
+        MockComponent
+      ]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MockComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
+
+  it('should create component', () => {
+    expect(component).toBeDefined();
+  });
+
+  it('should expand component', () => {
+    const debugEl: DebugElement = fixture.debugElement.query(By.css('input'));
+    const input: HTMLInputElement = debugEl.nativeElement;
+
+    const initialWidth: number = input.getBoundingClientRect().width;
+    input.value = 'Adding some text';
+    debugEl.triggerEventHandler('input', {
+      target: input
+    });
+    const resultWidth: number = input.getBoundingClientRect().width;
+
+    expect(resultWidth).toBeGreaterThan(initialWidth);
+  });
+
+  it('should shrink component', () => {
+    const debugEl: DebugElement = fixture.debugElement.query(By.css('input'));
+    const input: HTMLInputElement = debugEl.nativeElement;
+
+    input.value = 'Adding some text';
+    debugEl.triggerEventHandler('input', {
+      target: input
+    });
+    const initialWidth: number = input.getBoundingClientRect().width;
+    input.value = '';
+    debugEl.triggerEventHandler('input', {
+      target: input
+    });
+    const resultWidth: number = input.getBoundingClientRect().width;
+
+    expect(resultWidth).toBeLessThan(initialWidth);
+  });
+
+  // TODO: test placeholder
+  // TODO: test min-width
+  // TODO: test max-width
+  // TODO: test extra space
+  // TODO: test ngModel
+  // TODO: test not an input
 });
