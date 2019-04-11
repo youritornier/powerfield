7e763b322f5335fae65242d64a3214e9488cb3fb
Youri <youri@guacalab.tech>
2019-04-11 17:28:40 +0200

    ng generate component layout --project=powerfield-app

--- projects/powerfield-app/src/app/app.module.ts
+++ projects/powerfield-app/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout.component';
 import { MaterialModule } from './material/material.module';
 import { PagesModule } from './pages/pages.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
