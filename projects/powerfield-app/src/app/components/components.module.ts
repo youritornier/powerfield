f8b11fd4c323e7b74ef52665d0632387d1865c00
Youri <youri@guacalab.tech>
2019-04-11 17:23:27 +0200

    Add: components module and pages module

---
+++ projects/powerfield-app/src/app/components/components.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MaterialModule } from '../material/material.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MaterialModule
+  ],
+  exports: [],
+  providers: []
+})
+export class ComponentsModule {}
