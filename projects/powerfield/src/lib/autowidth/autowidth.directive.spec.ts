c4f034dc5ef10e727eb6b29380f46b9631053cec
Youri <youri@guacalab.tech>
2019-04-11 15:58:15 +0200

    Fix: add unit tests

--- projects/powerfield/src/lib/autowidth/autowidth.directive.spec.ts
+++ projects/powerfield/src/lib/autowidth/autowidth.directive.spec.ts
@@ -1,25 +1,31 @@
 import { Component, } from '@angular/core';
 import { async, TestBed } from '@angular/core/testing';
 
-import { ElementInputHelperFactory, ElementHelper } from '../testing';
+import { ElementInputHelper, ElementInputHelperFactory } from '../testing';
 import { AutowidthDirective } from './autowidth.directive';
+
+const powExtraWidth = 50;
+const powMaxWidth = 50;
+const powMinWidth = 100;
 
 @Component({
   template:
-    '<input id="autoWidth" type="text" value="first" powAutowidth>' +
-    '<input id="autoWidthExtra" type="text" value="first" powAutowidth [powExtraWidth]="50">' +
-    '<input id="autoWidthMax" type="text" powAutowidth [powMaxWidth]="50">' +
-    '<input id="autoWidthMin" type="text" powAutowidth [powMinWidth]="100">' +
-    '<input id="autoWidthPlaceholder" placeholder="This is a placeholder" powAutowidth>'
+    `<input id="autoWidth" type="text" value="text" powAutowidth>
+    <input id="autoWidthExtra" type="text" value="text" powAutowidth [powExtraWidth]="${powExtraWidth}">
+    <!-- #autoWidthMax is box-sizing: border-box; to verify max-width with padding and borders -->
+    <input id="autoWidthMax" type="text" value="text" style="box-sizing: border-box;" powAutowidth [powMaxWidth]="${powMaxWidth}">
+    <!-- #autoWidthMin is box-sizing: border-box; to verify min-width with padding and borders -->
+    <input id="autoWidthMin" type="text" value="text" style="box-sizing: border-box;" powAutowidth [powMinWidth]="${powMinWidth}">
+    <input id="autoWidthPlaceholder" placeholder="This is a placeholder" powAutowidth>`
 })
 class MockComponent { constructor() { } }
 
 describe('AutowidthDirective', () => {
-  let elHelper: ElementHelper<MockComponent, HTMLInputElement>;
-  let elExtraHelper: ElementHelper<MockComponent, HTMLInputElement>;
-  let elMaxHelper: ElementHelper<MockComponent, HTMLInputElement>;
-  let elMinHelper: ElementHelper<MockComponent, HTMLInputElement>;
-  let elPlaceholderHelper: ElementHelper<MockComponent, HTMLInputElement>;
+  let elHelper: ElementInputHelper<MockComponent>;
+  let elExtraHelper: ElementInputHelper<MockComponent>;
+  let elMaxHelper: ElementInputHelper<MockComponent>;
+  let elMinHelper: ElementInputHelper<MockComponent>;
+  let elPlaceholderHelper: ElementInputHelper<MockComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -49,34 +55,72 @@ describe('AutowidthDirective', () => {
 
   it('should expand component', () => {
     const initialWidth: number = elHelper.element.getBoundingClientRect().width;
-    elHelper.element.value = 'Adding some text';
-    elHelper.debugEl.triggerEventHandler('input', {
-      target: elHelper.element
-    });
+    elHelper.setInputValue('Adding some text');
     const resultWidth: number = elHelper.element.getBoundingClientRect().width;
 
     expect(resultWidth).toBeGreaterThan(initialWidth);
   });
 
   it('should shrink component', () => {
-    elHelper.element.value = 'Adding some text';
-    elHelper.debugEl.triggerEventHandler('input', {
-      target: elHelper.element
-    });
     const initialWidth: number = elHelper.element.getBoundingClientRect().width;
-    elHelper.element.value = '';
-    elHelper.debugEl.triggerEventHandler('input', {
-      target: elHelper.element
-    });
+    elHelper.setInputValue('');
     const resultWidth: number = elHelper.element.getBoundingClientRect().width;
 
     expect(resultWidth).toBeLessThan(initialWidth);
   });
 
-  // TODO: test placeholder
-  // TODO: test min-width
-  // TODO: test max-width
-  // TODO: test extra space
+  it('should be initialized with extra width', () => {
+    const width: number = elHelper.element.getBoundingClientRect().width;
+    const extraWidth: number = elExtraHelper.element.getBoundingClientRect().width;
+
+    expect(extraWidth).toEqual(width + powExtraWidth);
+  });
+
+  it('should expand with extra width', () => {
+    elHelper.setInputValue('Adding some text');
+    const width: number = elHelper.element.getBoundingClientRect().width;
+
+    elExtraHelper.setInputValue('Adding some text');
+    const extraWidth: number = elExtraHelper.element.getBoundingClientRect().width;
+
+    expect(extraWidth).toEqual(width + powExtraWidth);
+  });
+
+  it('should shrink with extra width', () => {
+    elHelper.setInputValue('');
+    const width: number = elHelper.element.getBoundingClientRect().width;
+
+    elExtraHelper.setInputValue('');
+    const extraWidth: number = elExtraHelper.element.getBoundingClientRect().width;
+
+    expect(extraWidth).toEqual(width + powExtraWidth);
+  });
+
+  it('should expand without exceeding the max width', () => {
+    elMaxHelper.setInputValue('Adding some very text');
+    const width: number = elMaxHelper.element.getBoundingClientRect().width;
+
+    // elMaxHelper has to be box-sizing: border-box;
+    // if not the powMaxWidth = width - border - padding
+    expect(width).toEqual(powMaxWidth);
+  });
+
+  it('should be initialized with the min width', () => {
+    const width: number = elMinHelper.element.getBoundingClientRect().width;
+
+    // elMinHelper has to be box-sizing: border-box;
+    // if not the powMinWidth = width - border - padding
+    expect(width).toEqual(powMinWidth);
+  });
+
+  it('should be initialized depending the placeholder width', () => {
+    const initialWidth: number = elPlaceholderHelper.element.getBoundingClientRect().width;
+    elPlaceholderHelper.setInputValue('Text');
+    const resultWidth: number = elPlaceholderHelper.element.getBoundingClientRect().width;
+
+    expect(initialWidth).toBeGreaterThan(resultWidth);
+  });
+
   // TODO: test ngModel
   // TODO: test not an input
 });
--- projects/powerfield/src/lib/testing/element.helper.factory.ts
+++ projects/powerfield/src/lib/testing/element.helper.factory.ts
@@ -2,18 +2,25 @@ import { DebugElement, Type } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 
-import { ElementHelper } from './element.helper';
+import { ElementHelper, ElementInputHelper } from './element.helper';
 
 /**
  * Abstract factory class to create an ElementHelper.
  */
 abstract class ElementHelperFactory<T, U extends HTMLElement> {
-  private readonly fixture: ComponentFixture<T>;
+  protected readonly fixture: ComponentFixture<T>;
 
   constructor(component: Type<T>) {
     this.fixture = TestBed.createComponent(component);
     this.fixture.detectChanges();
   }
+
+  /**
+   * Creates a new instance of an ElementHelper.
+   * @param debugEl the debug element
+   * @param element the element
+   */
+  protected abstract construct(debugEl: DebugElement, element: U): ElementHelper<T, U>;
 
   /**
    * Creates an ElementHelper instance based on the component.
@@ -23,7 +30,7 @@ abstract class ElementHelperFactory<T, U extends HTMLElement> {
     const debugEl: DebugElement = this.fixture.debugElement.query(By.css(selector));
     const element: U = debugEl.nativeElement;
 
-    return new ElementHelper(this.fixture.componentInstance, debugEl, element);
+    return this.construct(debugEl, element);
   }
 }
 
@@ -31,4 +38,21 @@ abstract class ElementHelperFactory<T, U extends HTMLElement> {
  * Factory class to create an ElementHelper with an HTMLInputElement.
  */
 export class ElementInputHelperFactory<T>
-  extends ElementHelperFactory<T, HTMLInputElement> { }
+  extends ElementHelperFactory<T, HTMLInputElement> {
+  /**
+   * Creates a new instance of an ElementInputHelper.
+   * @param debugEl the debug element
+   * @param element the element
+   */
+  protected construct(debugEl: DebugElement, element: HTMLInputElement): ElementInputHelper<T> {
+    return new ElementInputHelper(this.fixture.componentInstance, debugEl, element);
+  }
+
+  /**
+   * Creates an ElementHelper instance based on the component.
+   * @param selector the css selector to get the element.
+   */
+  public create(selector: string): ElementInputHelper<T> {
+    return super.create(selector) as ElementInputHelper<T>;
+  }
+}
