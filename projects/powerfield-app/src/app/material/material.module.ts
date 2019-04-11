e87a308110425b309febe995dcd8e3874656d1c2
Youri Tornier <youri.tornier@gmail.com>
2019-04-11 17:21:17 +0200

    Add: material module

---
+++ projects/powerfield-app/src/app/material/material.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  imports: [
+    BrowserAnimationsModule
+  ],
+  exports: [
+    BrowserAnimationsModule
+  ],
+  providers: []
+})
+export class MaterialModule { }
