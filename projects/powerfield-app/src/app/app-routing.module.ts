d0ecaec286c7120f854fc7dc643f79c8089fe490
Youri Tornier <youri.tornier@gmail.com>
2019-04-12 10:09:26 +0200

    Fix: routing home, quick start and page not found

--- projects/powerfield-app/src/app/app-routing.module.ts
+++ projects/powerfield-app/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { QuickstartComponent } from './pages/quickstart/quickstart.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'quickstart', component: QuickstartComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
