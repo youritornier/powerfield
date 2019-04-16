43bcfb0069d379e77e42a9603bbe1856e20770e9
Youri <youri@guacalab.tech>
2019-04-16 10:28:22 +0200

    Fix: build menu based on routing service

--- projects/powerfield-app/src/app/layout/layout.component.ts
+++ projects/powerfield-app/src/app/layout/layout.component.ts
@@ -1,6 +1,9 @@
 import { MediaMatcher } from '@angular/cdk/layout';
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 
+import { DocMenuModel } from '../models';
+import { RoutingService } from '../services/routing/routing.service';
+
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
@@ -9,17 +12,26 @@ import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 export class LayoutComponent implements OnInit {
   private mediaQueryListener: () => void;
 
+  public docMenu: Array<DocMenuModel>;
   public mediaQuery: MediaQueryList;
 
   constructor(
     changeDetectorRef: ChangeDetectorRef,
-    media: MediaMatcher
+    media: MediaMatcher,
+    private routingService: RoutingService
   ) {
     this.mediaQueryListener = () => changeDetectorRef.detectChanges();
     this.mediaQuery = media.matchMedia('(max-width: 990px)');
+    this.docMenu = new Array();
   }
 
   ngOnInit() {
     this.mediaQuery.addListener(this.mediaQueryListener);
+    this.initializeDocMenu();
+  }
+
+  private initializeDocMenu() {
+    this.routingService.getAll()
+      .subscribe(d => this.docMenu = d);
   }
 }
