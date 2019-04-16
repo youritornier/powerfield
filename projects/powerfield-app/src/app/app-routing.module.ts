ced9e491e9ef4811cf5e12cdfa21baf3006d8ecb
Youri <youri@guacalab.tech>
2019-04-16 10:28:04 +0200

    Add: docs component and generic routing

--- projects/powerfield-app/src/app/app-routing.module.ts
+++ projects/powerfield-app/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { DocsComponent } from './pages/docs/docs.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AutowidthComponent } from './pages/input/autowidth/autowidth.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
@@ -8,6 +9,7 @@ import { QuickstartComponent } from './pages/quickstart/quickstart.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'docs/:type/:name', component: DocsComponent },
   { path: 'input/autowidth', component: AutowidthComponent },
   { path: 'quickstart', component: QuickstartComponent },
   { path: '**', component: PageNotFoundComponent }
