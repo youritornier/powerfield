d470ef6fd90a9d586b1aa13243f0532f995d8600
Youri <youri@guacalab.tech>
2019-04-16 15:23:43 +0200

    Delete: refactoring dead code

--- projects/powerfield-app/src/app/pages/pages.module.ts
+++ projects/powerfield-app/src/app/pages/pages.module.ts
@@ -8,14 +8,12 @@ import { MaterialModule } from '../material/material.module';
 import { ServicesModule } from '../services/services.module';
 import { DocsComponent } from './docs/docs.component';
 import { HomeComponent } from './home/home.component';
-import { AutowidthComponent } from './input/autowidth/autowidth.component';
 import { AutowidthExampleComponent } from './input/autowidth/autowidth-example.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { QuickstartComponent } from './quickstart/quickstart.component';
 
 @NgModule({
   declarations: [
-    AutowidthComponent,
     AutowidthExampleComponent,
     DocsComponent,
     HomeComponent,
@@ -31,7 +29,6 @@ import { QuickstartComponent } from './quickstart/quickstart.component';
     ServicesModule
   ],
   exports: [
-    AutowidthComponent,
     DocsComponent,
     HomeComponent,
     PageNotFoundComponent,
