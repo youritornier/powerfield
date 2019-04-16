169e48631549452eaa181d45928f8ce866c8d13c
Youri <youri@guacalab.tech>
2019-04-16 10:15:16 +0200

    Add: adapter and routing service

--- projects/powerfield-app/src/app/models/index.ts
+++ projects/powerfield-app/src/app/models/index.ts
@@ -1,3 +1,4 @@
+export * from './doc-menu.model';
 export * from './documentation.model';
 export * from './example.model';
 export * from './property.model';
---
+++ projects/powerfield-app/src/app/services/routing/routing.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { DocMenuModel } from '../../models';
+import { RouteAdapter } from '../../adapters';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RoutingService {
+  private baseUrl = './assets/data.json';
+
+  constructor(
+    private http: HttpClient,
+    private adapter: RouteAdapter
+  ) { }
+
+  public getAll(): Observable<Array<DocMenuModel>> {
+    return this.http
+      .get(this.baseUrl)
+      .pipe(
+        map((data: any) => this.adapter.adaptArray(data.routes))
+      );
+  }
+}
