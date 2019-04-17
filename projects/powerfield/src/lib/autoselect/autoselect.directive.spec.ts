01c548505f9c95b2b6b76769c0f4c091c61ac11e
Youri <youri@guacalab.tech>
2019-04-17 10:52:11 +0200

    Add: autoselect directive

---
+++ projects/powerfield/src/lib/autoselect/autoselect.directive.spec.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { async, TestBed } from '@angular/core/testing';
+
+import { ElementInputHelper, ElementInputHelperFactory } from '../testing';
+import { AutoselectDirective } from './autoselect.directive';
+
+@Component({
+  template:
+    '<input id="autoSelect" type="text" value="This is a text" powAutoselect>'
+})
+class MockComponent {
+  constructor() { }
+}
+
+describe('AutoselectDirective', () => {
+  let elHelper: ElementInputHelper<MockComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        AutoselectDirective,
+        MockComponent
+      ]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    const elHelperFactory = new ElementInputHelperFactory(MockComponent);
+    elHelper = elHelperFactory.create('input#autoSelect');
+  });
+
+  it('should create component', () => {
+    expect(elHelper.component).toBeDefined();
+  });
+
+  it('should select all text', () => {
+    elHelper.debugEl.triggerEventHandler('click', {
+      target: elHelper.element
+    });
+    const selectedText = elHelper.element.value.substring(
+      elHelper.element.selectionStart,
+      elHelper.element.selectionEnd
+    );
+
+    expect(selectedText).toEqual(elHelper.element.value);
+  });
+});
